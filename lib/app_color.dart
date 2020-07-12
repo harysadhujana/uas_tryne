1c387013d141d3ce77eab01759af11946bd9cff4
harysadhujana <harysadhujana@gmail.com>
2020-07-12 20:24:01 +0800

    uas_tryne

---
+++ lib/app_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  final Color colorPrimary = Colors.redAccent;
+  final Color colorSecondary = Colors.red;
+  final Color colorTertiary = Colors.redAccent;
+}
